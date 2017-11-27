b7fcf00329005234c3c24ad8dfae38c06f1fafb6
Siqi Yan <syan3@ualberta.ca>
2017-11-27 10:53:41 -0700

    fixed single word and single phrase parsing

--- dataRetrieval.py
+++ dataRetrieval.py
@@ -14,6 +14,22 @@ def search_db(db, expr, keep_index=None):
         item = cursor.next_dup()
     return result
 
+
+def find_and_remove(query, regex):
+    match = re.findall(regex, query)
+    query = re.sub(regex, '', query)
+    return match, query
+
+
+def format_to_key(match):
+    result = []
+    for each in match:
+        for word in each.split():
+            word = re.sub(r'[:\-\"]', '', word)
+            result.append(word)
+    return result
+
+
 class DataRetrieval:
     def __init__(self):
         self.recs_db = db.DB()
@@ -45,7 +61,8 @@ class DataRetrieval:
             return 
 
         # Years:
-        match = re.findall(r'year *([\<\>\:]) *([0-9]+)', query)
+        # match = re.findall(r'year *([\<\>\:]) *([0-9]+)', query)
+        match, query = find_and_remove(query, r'year *([\<\>\:]) *([0-9]+)')
         global_result = None
         # global_result = set()
         for sign, year in match:
@@ -62,53 +79,112 @@ class DataRetrieval:
             else:
                 global_result &= set(result)
 
+
+
+        # Title:
+        # TODO: BUG. Name cannot contains characters like &eacute
+        # match = re.findall(r'title *: *\"*([\w \:_\-]*)\"', query)
+        # match += re.findall(r'title *: *([\w_\-]+)', query)
+        # match1, query = find_and_remove(query, r'title *: *\"*([\w \:_\-]*)\"')
+        match1, query = find_and_remove(query, r'title *: *(\"*[\w \:_\-]+\")')
+        match2, query = find_and_remove(query, r'title *: *([\w_\-]+)')
+        # match += single_match
+        for word in format_to_key(match1 + match2):
+            result = self.search_title(word)
+            if global_result == None:
+                global_result = set(result)
+            else:
+                global_result &= set(result)
+
+        # for title in match1 + match2:
+            # for word in title.split():
+                # word = re.sub('[:\-]', '', word)
+                # result = search_db(self.terms_db, ('t-'+word).encode(), keep_index=1)
+                # if global_result == None:
+                    # global_result = set(result)
+                # else:
+                    # global_result &= set(result)
+
+        # Author:
+        # TODO: BUG. Name cannot contains characters like &eacute
+        # match = re.findall(r'author *: *(\w+)', query)
+        # match += re.findall(r'author *: *\"([\w \.]*)\"', query)
+        match1, query = find_and_remove(query, r'author *: *(\w+)')
+        match2, query = find_and_remove(query, r'author *: *\"([\w \.]*)\"')
+        # match += single_match
+        for word in format_to_key(match1 + match2):
+            result = self.search_author(word)
+            if global_result == None:
+                global_result = set(result)
+            else:
+                global_result &= set(result)
+
+        # for author in match1 + match2:
+            # for word in author.split():
+                # print(word)
+                # result = search_db(self.terms_db, ('a-'+word).encode(), keep_index=1)
+                # print('result=', result)
+                # if global_result == None:
+                    # global_result = set(result)
+                # else:
+                    # global_result &= set(result)
+
+        # Other:
+        # match = re.findall(r'other *: *(\w+)', query)
+        match1, query = find_and_remove(query, r'other *: *(\w+)')
+        # match += re.findall(r'other *: *\"([\w ]*)\"', query)
+        match2, query = find_and_remove(query, r'other *: *\"([\w ]*)\"')
+        # match += single_match
+        for word in format_to_key(match1 + match2):
+            result = self.search_other(word)
+            if global_result == None:
+                global_result = set(result)
+            else:
+                global_result &= set(result)
+        # for other in match:
+            # for word in other.split():
+                # word = re.sub('[:\-]', '', word)
+                # result = search_db(self.terms_db, ('o-'+word).encode(), keep_index=1)
+                # if global_result == None:
+                    # global_result = set(result)
+                # else:
+                    # global_result &= set(result)
+
         # No prefex:
         # TODO: not finished
         # match = re.findall(r' (\w+) ', query)
         # match += re.findall(r'(\w+)', query)
         # match += re.findall(r'\"([\w ]*)\"', query)
         # single_match = match
+        # single = re.sub(r'output\s*=\s*(key|full)', '', query)
+        # single = re.sub(r'year *([\<\>\:]) *([0-9]+)', '', single)
+        # single = re.sub(r'title *: *\"*([\w \:_\-]*)\"', '', single)
+        # single = re.sub(r'title *: *([\w_\-]+)', '', single)
+        # single = re.sub(r'author *: *(\w+)', '', single)
+        # single = re.sub(r'author *: *\"([\w \.]*)\"', '', single)
+        # single = re.sub(r'other *: *(\w+)', '', single)
+        # single = re.sub(r'other *: *\"([\w ]*)\"', '', single)
+        # single = single.split()
+        match1, query = find_and_remove(query, r'(\".+\")')
+        query = re.sub(r'\w+:\w*', '', query)
+        single = query.split() + match1
         single_match = []
+        for each in single:
+            if ':' not in each:
+                single_match.append(each)
 
-        # Title:
-        # TODO: BUG. Name cannot contains characters like &eacute
-        match = re.findall(r'title *: *\"*([\w \:_\-]*)\"', query)
-        match += re.findall(r'title *: *([\w_\-]+)', query)
-        match += single_match
-        for title in match:
-            for word in title.split():
-                word = re.sub('[:\-]', '', word)
-                result = search_db(self.terms_db, ('t-'+word).encode(), keep_index=1)
-                if global_result == None:
-                    global_result = set(result)
-                else:
-                    global_result &= set(result)
+        keys = format_to_key(single_match)
+        for word in keys:
+            single_title = self.search_title(word)
+            single_author = self.search_author(word)
+            single_other = self.search_other(word)
 
-        # Author:
-        # TODO: BUG. Name cannot contains characters like &eacute
-        match = re.findall(r'author *: *(\w+)', query)
-        match += re.findall(r'author *: *\"([\w \.]*)\"', query)
-        match += single_match
-        for author in match:
-            for word in author.split():
-                result = search_db(self.terms_db, ('a-'+word).encode(), keep_index=1)
-                if global_result == None:
-                    global_result = set(result)
-                else:
-                    global_result &= set(result)
-
-        # Other:
-        match = re.findall(r'other *: *(\w+)', query)
-        match += re.findall(r'other *: *\"([\w ]*)\"', query)
-        match += single_match
-        for other in match:
-            for word in other.split():
-                word = re.sub('[:\-]', '', word)
-                result = search_db(self.terms_db, ('o-'+word).encode(), keep_index=1)
-                if global_result == None:
-                    global_result = set(result)
-                else:
-                    global_result &= set(result)
+        if len(keys) > 0:
+            single_result = set(single_title) | set(single_author) | set(single_other)
+            if global_result == None:
+                global_result = single_result
+            else:
+                global_result &= single_result
 
 
         if global_result is None:
@@ -145,6 +221,15 @@ class DataRetrieval:
                 result.append(item[keep_index])
             item = cursor.next()
         return result
+
+    def search_title(self, word):
+        return search_db(self.terms_db, ('t-'+word).encode(), keep_index=1)
+
+    def search_author(self, word):
+        return search_db(self.terms_db, ('a-'+word).encode(), keep_index=1)
+
+    def search_other(self, word):
+        return search_db(self.terms_db, ('o-'+word).encode(), keep_index=1)
 
     def close(self):
         self.recs_db.close()
